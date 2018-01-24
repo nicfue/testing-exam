94f4e19a6b7ae749e15b2c83102ac301d474acc6
petersandqvist <petersandqvist@Peters-MacBook.local>
2018-01-24 12:45:59 +0100

    changes

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -8,3 +8,26 @@ it('should render <App /> without user', () => {
   const user = wrapper.state().currentPersona;
   expect(user).toBe('');
 });
+
+it('Render <App /> with user', () => {
+  const wrapper = shallow(<App />);
+  const persona = 'Nisse';
+  wrapper.setState({'currentPersona': persona});
+  const user = wrapper.state().currentPersona;
+  expect(user).toBe('Nisse');
+});
+
+it('Render App default page', () => {
+  const wrapper = shallow(<App />); 
+  const { currentPage } = wrapper.state();
+  expect(currentPage).toBe('home');
+});
+
+it('Change page from home to bot', () => {
+  const wrapper = mount(<App />);
+  const { currentPage:defaultPage} = wrapper.state();
+  expect(defaultPage).toBe('home');
+  wrapper.instance(defaultPage).changePage();
+  const { currentPage } = wrapper.state();
+  expect(currentPage).toBe('bot');
+});
