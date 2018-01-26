bc5fdd034dfb280af83437079e658ae74207343e
nicfue <nicolasfuentes@hotmail.com>
2018-01-26 14:56:28 +0100

    adding some unit tests and snapshots to SinglePost

--- src/__tests__/SinglePost.js
+++ src/__tests__/SinglePost.js
@@ -1,36 +1,52 @@
 import React from 'react';
 import { mount, shallow, render } from 'enzyme';
 import SinglePost from '../components/SinglePost';
-import Posts from '../components/Posts';
-import SingleComment from '../components/SingleComment';
 
-
-it('if author and currentPersona is the same, then the button should render', () => {
-  const mock = jest.fn();
-  const wrapper = mount(<SinglePost
+describe('Unit test on SinglePost', ()=>{
+  const post = jest.fn();
+  const wrapper1 = mount(<SinglePost
     title="heading"
     content="text"
     id="testId"
     author="Zac"
     currentPersona="Zac"
     date="0000000"
-    onClick={mock}
+    onClick={post}
     />)
 
-  expect(wrapper.find('Button').length).toEqual(1);
+    const wrapper2 = mount(<SinglePost
+      title="heading"
+      content="text"
+      id="testId"
+      author="Zac"
+      currentPersona="Morgana"
+      date="0000000"
+      onClick={post}
+      />)
+
+it('if author and currentPersona is the same, then the button should render', () => {
+  expect(wrapper1.find('Button').length).toEqual(1);
 })
 
 it('if author and currentPersona are not the same, then the button should not render', () => {
-  const mock = jest.fn();
-  const wrapper = mount(<SinglePost
-    title="heading"
-    content="text"
-    id="testId"
-    author="Zac"
-    currentPersona="Esmeralda"
-    date="0000000"
-    onClick={mock}
-    />)
+  expect(wrapper2.find('Button').length).toEqual(0);
+ })
 
-  expect(wrapper.find('Button').length).toEqual(0);
+ it('snapshots when match', ()=> {
+   expect(wrapper1).toMatchSnapshot();
+ })
+
+ it('snapshots when mismatch', ()=> {
+   expect(wrapper2).toMatchSnapshot();
+ })
+
+ it('posts should contain a title and content', () => {
+   expect(wrapper1.find('article h2').text()).toEqual('heading')
+ })
+
+ it('test onclick with id', ()=>{
+   wrapper1.find('button').simulate('click');
+   expect(post).toBeCalledWith('testId');
+ });
+
 })
