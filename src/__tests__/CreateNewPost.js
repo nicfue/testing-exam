cebd3bff06559ebb27c12f94e0b476bc834f067f
nicfue <nicolasfuentes@hotmail.com>
2018-01-26 16:35:32 +0100

    testing CreateNewPost

--- src/__tests__/CreateNewPost.js
+++ src/__tests__/CreateNewPost.js
@@ -2,6 +2,30 @@ import React from 'react';
 import { mount, shallow, render } from 'enzyme';
 import CreateNewPost from '../components/CreateNewPost';
 
-test.skip('renders the app', () => {
-  render(<CreateNewPost />);
-});
+
+describe('testing create new post', () => {
+  const newPost = jest.fn();
+  const wrapper = mount(<CreateNewPost author="" updatePosts={newPost} />);
+
+  it('should create a new title', ()=>{
+    const title = wrapper.find("#title");
+    title.simulate('change', {target: { name: 'title', value: 'testTitle'}});
+    expect(wrapper.state().title).toEqual('testTitle');
+    });
+
+  it('should create new content', ()=>{
+    const content = wrapper.find("#content")
+    content.simulate('change', {target: { name: 'content', value: 'testContent'}});
+    expect(wrapper.state().content).toEqual('testContent');
+    });
+
+  it('testing submit', () => {
+    const submit = wrapper.find("form")
+    submit.simulate('submit')
+  })
+
+  it('snapshots when match', ()=> {
+    expect(wrapper).toMatchSnapshot();
+  })
+
+})
