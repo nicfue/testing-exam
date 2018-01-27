1451eea6af176176d6357c806b955fd3c87a9ebf
petersandqvist <petersandqvist@Peters-MacBook.local>
2018-01-27 13:32:20 +0100

    messageform

--- src/__tests__/Bot/MessageForm.js
+++ src/__tests__/Bot/MessageForm.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { mount, shallow, render } from 'enzyme';
-import MessageForm from '../Bot/MessageForm';
+import MessageForm from "../../components/Bot/MessageForm";
 
-test.skip('renders the app', () => {
-  render(<MessageForm />);
+it('simulate submit', () => {
+  const fakeSubmit= jest.fn()
+  const wrapper = mount(<MessageForm userMessage={fakeSubmit} onSubmit={fakeSubmit} />);
+  wrapper.find("form").simulate("submit",{  fakeSubmit  });
+  expect(fakeSubmit).toHaveBeenCalled();
+  
 });
