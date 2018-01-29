2dee519f4797e34c9a16bf2a3e52e4d63634d10c
petersandqvist <petersandqvist@Peters-MacBook.lan>
2018-01-29 10:57:23 +0100

    create new comment

--- src/__tests__/Bot/Bot.js
+++ src/__tests__/Bot/Bot.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { mount, shallow, render } from 'enzyme';
 import Bot from '../../components/Bot/Bot';
 
-it('renders message on screen', () => {
+it('renders the message on the screen', () => {
   const wrapper = shallow(<Bot />);
   wrapper.instance().onSubmit('Message');
   expect(wrapper.html()).toContain('Message');
 });
+
+it.skip('WHAT TO DO?', () => {
+  const wrapper = shallow(<Bot />);
+
+})
--- src/__tests__/CreateNewComment.js
+++ src/__tests__/CreateNewComment.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { mount, shallow, render } from 'enzyme';
 import CreateNewComment from '../components/CreateNewComment';
 
-test.skip('renders the app', () => {
-  render(<CreateNewComment />);
-});
+const newComment = mount(<CreateNewComment author='Esmeralda' postId="1" updateComments={() => { }} />);
+
+it('create new comment', () => {
+  newComment.find('textarea[name="comment"]').simulate('change', { target: { name: 'comment', value: 'ny kommentar' } });
+  expect(newComment.state().comment).toContain('ny kommentar');
+})
